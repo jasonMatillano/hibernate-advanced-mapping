8b8cf0f895bcd3cfefdbcbe5b05451abf7f2e7c4
jasonMatillano <jason.matillano@gmail.com>
2018-09-17 19:13:00 +0800

    eager

---
+++ src/com/luv2code/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,51 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+						.configure("hibernate.cfg.xml")
+						.addAnnotatedClass(Instructor.class)
+						.addAnnotatedClass(InstructorDetail.class)
+						.addAnnotatedClass(Course.class)
+						.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start transaction
+			session.beginTransaction();
+			
+			// get instructor from db
+			int theId = 2;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			// get course for the instructor
+			System.out.println("luv2code: Instructor: " + tempInstructor);
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("luv2code: Done!");
+			
+		} finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close(); 
+		}
+	}
+}
