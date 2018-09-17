d86d64540d6ee04f91b04151408b3fcbd4f05980
jasonMatillano <jason.matillano@gmail.com>
2018-09-17 18:01:15 +0800

    retrieve courses from instructor

--- src/com/luv2code/hibernate/demo/GetInstructorCoursesDemo.java
+++ src/com/luv2code/hibernate/demo/GetInstructorCoursesDemo.java
@@ -8,7 +8,7 @@ import com.luv2code.hibernate.demo.entity.Course;
 import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
 
-public class CreateCoursesDemo {
+public class GetInstructorCoursesDemo {
 
 	public static void main(String[] args) {
 		
@@ -31,17 +31,9 @@ public class CreateCoursesDemo {
 			int theId = 1;
 			Instructor tempInstructor = session.get(Instructor.class, theId);
 			
-			// create some courses
-			Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide3");
-			Course tempCourse2 = new Course("The Pinball MasterClass3");
-			
-			// add courses to instructor
-			tempInstructor.add(tempCourse1);
-			tempInstructor.add(tempCourse2);
-			
-			// save the courses
-			session.save(tempCourse1);
-			session.save(tempCourse2);
+			// get course for the instructor
+			System.out.println("Instructor: " + tempInstructor);
+			System.out.println("Courses: " + tempInstructor.getCourses());
 			
 			// commit the transaction
 			session.getTransaction().commit();
