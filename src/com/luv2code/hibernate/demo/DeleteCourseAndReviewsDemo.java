b7ce6adf7fcf4f1f5608dcc01853c18c8937214e
jasonMatillano <jason.matillano@gmail.com>
2018-09-18 17:56:16 +0800

    delete

---
+++ src/com/luv2code/hibernate/demo/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,56 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+
+public class DeleteCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+						.configure("hibernate.cfg.xml")
+						.addAnnotatedClass(Instructor.class)
+						.addAnnotatedClass(InstructorDetail.class)
+						.addAnnotatedClass(Course.class)
+						.addAnnotatedClass(Review.class)
+						.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start transaction
+			session.beginTransaction();
+			
+			// get course from db
+			int theId = 14;
+			Course tempCourse = session.get(Course.class, theId);
+			
+			// get reviews form course
+			System.out.println("luv2code tempCourse : " + tempCourse);
+			System.out.println("luv2code tempCourseReviews : " + tempCourse.getReviews());
+			
+			// delete the course
+			session.delete(tempCourse);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		} finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close(); 
+		}
+	}
+}
